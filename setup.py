7131405ccc7422679cfe20a88274412ac469dddf
agitama0006 <112637625+agitama0006@users.noreply.github.com>
2022-09-01 13:54:40 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi68.py",
+    version = "0.1",
+    py_modules = ['agi68'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
